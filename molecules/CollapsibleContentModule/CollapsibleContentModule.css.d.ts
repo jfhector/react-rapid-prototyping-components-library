bb049c877534cb020f56a3299dfbc742564ec96f
Jean-Francois Hector <jeanfrancois.hector@googlemail.com>
2018-06-27 11:56:44 +0100

    finalised for first upload to github

---
+++ molecules/CollapsibleContentModule/CollapsibleContentModule.css.d.ts
@@ -0,0 +1,5 @@
+export const CollapsibleContentModule: string;
+export const collapseButtonContainer: string;
+export const title: string;
+export const childrenContainer: string;
+export const expanded: string;
